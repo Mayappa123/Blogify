8109a634fcea709a7a931e58dd96df2a2dcf4ed0
Mayappa <mayappapujari561999@gmail.com>
2024-05-18 21:02:26 +0530

    removed unwanted code

--- config.js
+++ config.js
@@ -6,4 +6,3 @@ const User = require("../models/User");
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
