c49374ffd3c4bf6c875d616770d9e065e12dcede
Mayappa <mayappapujari561999@gmail.com>
2024-05-25 21:05:11 +0530

    fixed some issues

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  console.log(req.user ? `currUser: ${req.user}` : "No user");
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
